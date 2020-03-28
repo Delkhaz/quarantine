b94704a0065afecb7b736f6cca712075b839e464
Delkhaz <Delkhazibrahimi@gmail.com>
2020-03-28 17:39:17 -0500

    song

---
+++ backend/Song.py
@@ -0,0 +1,12 @@
+import time
+
+class Song():
+    def __init__():
+        self.artist = None
+        self.song = None
+        self.album = None
+        self.year = None
+        self.album_art = None
+        self.time_added = int(time.time())
+
+
