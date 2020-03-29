98998eae8bced8ada1463215ecca8f21a37a4bde
Delkhaz <Delkhazibrahimi@gmail.com>
2020-03-28 22:07:14 -0500

    controller

--- backend/Controller.py
+++ backend/Controller.py
@@ -1,29 +1,57 @@
 from collections import defaultdict
-import requests
-import json
-import bs4
+from Parse import Parse
 
-
-# caching the page so it doesnt get load so much
-page_cache = defaultdict()
-
-def cache(url):
-    if url not in page_cache:
-        respone = requests.get(url)
-        page_cache[url] = bs4.BeautifulSoup(respone.text)
-    return page_cache[url]
-
+#song info is artist name + song title essentially 
 class Controller():
 
     def __init__(self):
         self.count = defaultdict()
-        self.songs = defaultdict()
+        self.songs_db = defaultdict()
         self.queue = []
         return
 
     def add(self, song):
-        pass
+        if self.queue:
+            prev = str(self.queue[0]['song_info'])
+        else:
+            prev = 'None'
+        
+        #song data is artist + song name
+        song_data = song['song_info']
+        print("Adding {} to the queue".format(song_data))
 
+        if song_data not in self.count:
+            self.count[song_data] = 0
+        self.count[song_data] += 1
+
+        if song_data not in self.songs_db:
+            self.songs_db[song_data] = song
+
+        while self.queue:
+            self.queue.pop()
+
+        # say you added papaoutai twice and tous les même once
+        # count list would be [2,1]
+        # not to be confused with self.count dict
+        counts_list = sorted(list(set(self.count.values())))[::-1]
+        #print(counts_list)
+
+        while counts_list:
+            temp = []
+            for artist_song, song_data in self.songs_db.items():
+                if self.count[artist_song] == counts_list[0]:
+                    temp.append(song_data)
+
+            counts_list.pop(0)
+            temp.sort(key=lambda l: l['time_added'])
+
+            for s in temp:
+                self.queue.append(s)
+
+            if str(self.queue[0]['song_info']) != prev:
+                print("order has been changed: {} is now next".format(self.queue[0]['song_info']))
+
+            
     def play_next(self):
         pass
 
@@ -37,5 +65,19 @@ class Controller():
         pass
 
 
+
 def main():
-    pass
+    song_list = []
+    song_list.append('https://open.spotify.com/track/1GC1MIaRMW3kfVK9VyD5Ii?si=W9SEsuTcTH2zmgabE_8ajA')
+    song_list.append('https://open.spotify.com/track/1QFw2xxyQtgKjlrMCEqsNj?si=PC7TJMbUS7Sy-jJycXOueg')
+    song_list.append('https://open.spotify.com/track/1GC1MIaRMW3kfVK9VyD5Ii?si=W9SEsuTcTH2zmgabE_8ajA')
+
+    song_controller = Controller()
+    for songs in song_list:
+        song_info = Parse(songs)
+        song_controller.add(vars(song_info))
+
+main()
+
+
+
