3400d497040c8726afb5d4ef0b249823bbaee882
Delkhaz <Delkhazibrahimi@gmail.com>
2020-03-28 22:08:10 -0500

    def song class

--- backend/Song.py
+++ backend/Song.py
@@ -1,12 +1,13 @@
 import time
 
 class Song():
-    def __init__():
+    def __init__(self):
         self.artist = None
         self.song = None
         self.album = None
         self.year = None
         self.album_art = None
-        self.time_added = int(time.time())
+        self.time_added = None
+        self.song_info = None
 
 
