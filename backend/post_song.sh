ea91967bf47c51ed4544199547bd89e1353c72ec
Delkhaz <Delkhazibrahimi@gmail.com>
2020-03-29 22:34:51 -0500

    frontend added lets build out player now

---
+++ backend/post_song.sh
@@ -0,0 +1,1 @@
+curl -i -H "Content-Type: application/json" -X POST -d '{"userId":"1", "url": "https://open.spotify.com/track/0rZtdLa27w0q2GJkLBX5SA?si=ABPCEYIkTJeqXBmA1kE61g"}' http://0.0.0.0:5000/post_song
